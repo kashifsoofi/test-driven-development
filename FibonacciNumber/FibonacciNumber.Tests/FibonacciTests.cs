7608ea084af5742779b2302d2bd8c06b439d3363
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2016-07-23 17:26:11 +0100

    Fibonacci Number Kata

---
+++ FibonacciNumber/FibonacciNumber.Tests/FibonacciTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+namespace FibonacciNumber.Tests
+{
+    [TestFixture()]
+    public class FibonacciTests
+    {
+        [Test]
+        public void Fib_Given0_Return0()
+        {
+            // arrange
+            int n = 0;
+
+            // act
+            int actual = Fibonacci.Fib(n);
+
+            // assert
+            Assert.That(0, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Fib_Given1_Return1()
+        {
+            // arrange
+            int n = 1;
+
+            // act
+            int actual = Fibonacci.Fib(n);
+
+            // assert
+            Assert.That(1, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Fib_Given2_Return1()
+        {
+            // arrange
+            int n = 2;
+
+            // act
+            int actual = Fibonacci.Fib(n);
+
+            // assert
+            Assert.That(1, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Fib_Given3_Return2()
+        {
+            // arrange
+            int n = 3;
+
+            // act
+            int actual = Fibonacci.Fib(n);
+
+            // assert
+            Assert.That(2, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void Fib_Given4_Return3()
+        {
+            // arrange
+            int n = 4;
+
+            // act
+            int actual = Fibonacci.Fib(n);
+
+            // assert
+            Assert.That(3, Is.EqualTo(actual));
+        }
+    }
+}
+
---
+++ FibonacciNumber/FibonacciNumber/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FibonacciNumber
+{
+    public class Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n <= 2)
+            {
+                return 1;
+            }
+
+            return Fib(n-1) + Fib(n-2);
+        }
+    }
+}
+
