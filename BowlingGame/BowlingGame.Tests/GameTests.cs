5e7927155dd6f9954d42f2cf5ee3572169afeafb
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2016-07-23 18:42:20 +0100

    Bowling Game Kata

---
+++ BowlingGame/BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System;
+namespace BowlingGame.Tests
+{
+    [TestFixture()]
+    public class GameTests
+    {
+        private Game g;
+
+        [SetUp]
+        public void SetUp()
+        {
+            g = new Game();
+        }
+
+        [Test]
+        public void DoesGameExists()
+        {
+            // arrange
+            // act
+            // assert
+            Assert.NotNull(g);
+        }
+
+        [Test]
+        public void GutterGameReturns0()
+        {
+            // arrange
+
+            // act
+            rollMany(20, 0);
+            int actual = g.scoreGame();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SinglePinGameReturns20()
+        {
+            // arrange
+
+            // act
+            rollMany(20, 1);
+            int actual = g.scoreGame();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void OneSpareReturnsAppropriateValue()
+        {
+            // arrange
+            g.roll(5);
+            g.roll(5); // spare
+            g.roll(3);
+
+            // act
+            rollMany(17, 0);
+            int actual = g.scoreGame();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void OneStrikeReturnsAppropriateValue()
+        {
+            // arrange
+            g.roll(10); // strike
+            g.roll(3);
+            g.roll(4);
+
+            // act
+            rollMany(16, 0);
+            int actual = g.scoreGame();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(24));
+        }
+
+        [Test]
+        public void PerfectGameReturns300()
+        {
+            // arrange
+
+            // act
+            rollMany(12, 10);
+            int actual = g.scoreGame();
+
+            // assert
+            Assert.That(actual, Is.EqualTo(300));
+        }
+
+        public void rollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.roll(pins);
+            }
+        }
+    }
+}
+
---
+++ BowlingGame/BowlingGame/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace BowlingGame
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void roll(int pins)
+        {
+            rolls[currentRoll] = pins;
+            currentRoll++;
+        }
+
+        public int scoreGame()
+        {
+            int score = 0;
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isStrike(frameIndex))
+                {
+                    score += 10 + strikeBonus(frameIndex);
+                    frameIndex++;
+                }
+                else if (isSpare(frameIndex))
+                {
+                    score += 10 + rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        public bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+        }
+
+        public bool isStrike(int frameIndex)
+        {
+            return rolls[frameIndex] == 10;
+        }
+
+        public int strikeBonus(int frameIndex)
+        {
+            return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+        }
+    }
+}
+
